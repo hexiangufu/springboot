82cd896cdc3ec160822f3eeab4babb665f6c0d7d
kk <javaengineerkang@163.com>
2017-07-26 16:58:05 +0800

    add config server project

--- springboot/src/main/java/com/kang/controller/UserController.java
+++ springboot/src/main/java/com/kang/controller/UserController.java
@@ -3,9 +3,11 @@ package com.kang.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kang.module.User;
 import com.kang.service.UserService;
@@ -16,10 +18,18 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Value("${spring.datasource.url}") String url;
+	
 	@RequestMapping("/")
 	String home(Model model){
 		List<User> userList = userService.queryAll();
 		model.addAttribute("users", userList);
 		return "user";
+	}
+	
+	@RequestMapping("/config")
+	@ResponseBody
+	String config(){
+		return url;
 	}
 }
